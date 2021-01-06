0500313ccee8a72f3ce0f86d75c5a0adeaaf0c71
Rychina Irina <ry4ina.irina@yandex.ru>
2021-01-07 00:19:27 +0500

    10/01/2021

--- index.php
+++ index.php
@@ -1,65 +1,72 @@
 <?php
+//1. Создайте функцию, которая принимает одномерный массив и
+// возвращает массив, заполненный случайными числами.
 
-//1. Дана строка. Если в этой строке более 5-ти символов - вырежете из нее первые 5 символов,
-//добавьте троеточие в конец и выведите на экран.
-//Если же в этой строке 5 и менее символов - просто выведите эту строку на экран.
-
-$str11 = "dialog!";
-$str12 = "mir";
-function five_symbols($str)
+function rand_array(&$input_array)
 {
-    if (mb_strlen($str) > 5)
+    foreach ($input_array as &$value)
     {
-        echo mb_substr($str, 0, 5) . '...';
+        $value = rand(0, 100);
     }
-    else {
-        echo $str;
+    return $input_array;
+}
+$example_array1 = [0, 2, 4, 6, 8, 10];
+print_r(rand_array($example_array1));
+
+
+echo '<hr>';
+//2. Дана строка «HTML, CSS, PHP, BITRIX».
+// Написать функцию, которая посчитает количество слов строке.
+
+$str2 = "HTML, CSS, PHP, BITRIX";
+function get_words_number($input_string)
+{
+    return count(explode(' ', $input_string));
+}
+echo get_words_number($str2);
+
+
+
+echo '<hr>';
+//3. Дана строка «HTML, CSS, PHP, BITRIX».
+//Написать функцию, которая выведет в обратном порядке буквы («XIRTIB ,PHP … »).
+
+$str3 = "HTML, CSS, PHP, BITRIX";
+function print_reverse_string($input_string)
+{
+    $input_string = str_split($input_string);
+    $input_string = array_reverse($input_string);
+    $input_string = implode("", $input_string);
+    echo $input_string;
+}
+print_reverse_string($str3);
+
+
+echo '<hr>';
+//4. Дана строка «HTML, CSS, PHP, BITRIX».
+// Написать функцию, которая выведет на экран длину строки.
+
+$str4 = "HTML, CSS, PHP, BITRIX";
+function get_symbols_number($input_string)
+{
+    $input_string_to_array = str_split($input_string);
+    return count($input_string_to_array);
+}
+echo get_symbols_number($str4);
+
+
+echo '<hr>';
+
+//5. Дана строка «HTML, CSS, PHP, BITRIX».
+// Написать функцию, которая выведет каждую букву на новую строку
+$str5 = "HTML, CSS, PHP, BITRIX";
+function vertical_string($input_string)
+{
+    $input_string_to_array = str_split($input_string);
+    foreach ($input_string_to_array as $value)
+    {
+        echo $value . '<br>';
     }
-}
-five_symbols($str11);
-echo '<br>';
-five_symbols($str12);
 
-echo '<hr>';
-//2. Дана строка $str. Замените в ней все буквы 'a' на цифру 1,
-// буквы 'b' - на 2, а буквы 'c' - на 3.
-$str2 = "analog bubble class";
-function replace_symbols($str)
-{
-    echo str_replace(array('a', 'b', 'c'), array(1, 2, 3), $str);
 }
-replace_symbols($str2);
-echo '<hr>';
-
-//3. Дана строка 'abc abc abc'. Определите позицию последней буквы 'b'.
-$str3 = 'abc abc abc';
-function find_position($str)
-{
-    return strpos($str, 'b', -4);
-}
-$position = find_position($str3);
-echo $position;
-
-echo '<hr>';
-//4.Дана строка 'html css php'.
-// С помощью функции explode запишите каждое слово этой строки в отдельный элемент массива.
-$str4 = 'html css php';
-function explode_example($str)
-{
-    return explode(' ', $str);
-}
-$arr4 = explode_example($str4);
-print_r($arr4);
-
-echo '<hr>';
-//5. В двух строках содержатся даты вида
-// День-Месяц-Год (например, 10-02-2015).
-// Определите количество дней между датами.
-$str51 = new DateTime('10-02-2015');
-$str52 = new DateTime('10-02-2016');
-function find_difference($str1, $str2)
-{
-    return $str1->diff($str2);
-}
-$interval = find_difference($str51, $str52);
-echo $interval->format('%R%a дней');
+vertical_string($str5);
