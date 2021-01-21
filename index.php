fbe5704c36c997c03847e053c2e90492ee5afce4
Rychina Irina <ry4ina.irina@yandex.ru>
2021-01-21 22:48:46 +0500

    21/01/2021

--- index.php
+++ index.php
@@ -1,26 +1,31 @@
 <?php
-//2. Дан файл style.css, в котором указаны цвета (созданы правила для фона, для блоков и т.д.).
-//Необходимо найти цвет в style.css и перенсти его на страницу index.php.
-//На странице index.php окрасить блоки или сделать выпадающий список цветом.
+$hostname = 'localhost';
+$username = 'Irina';
+$password = '1234';
+$dbname = 'People';
 
-$filename = "style.css";
-$handle = fopen($filename, "r");
-$contents = fread($handle, 1);
-$colors_array = [];
-while ($contents != null)
-{
-    if ($contents == '#'){
-        $colors_array[] = $contents . fread($handle, 6);
-    }
-    $contents = fread($handle, 1);
-}
-print($colors_array[0]);
-print($colors_array[1]);
-print($colors_array[2]);
-?>
+$dbconnect = mysqli_connect($hostname, $username, $password, $dbname);
+$sql = mysqli_query($dbconnect, "CREATE DATABASE $dbname");
+$create_hobbies = mysqli_query($dbconnect, "CREATE TABLE Hobbies(
+    id_hobbies int (5) AUTO_INCREMENT PRIMARY KEY,
+    Name varchar (30) NOT NULL,
+    Desription varchar(30) NOT NULL)"
+);
+$create_people_hobbies = mysqli_query($dbconnect, "CREATE TABLE People_hobbies(
+    id_record int (5) AUTO_INCREMENT PRIMARY KEY,
+    id_people int (5),
+    id_hobbies int (5))"
+);
+$insert_people1 = mysqli_query($dbconnect, "INSERT INTO `people`(`id_person`, `Name`, `Surname`, `Age`) VALUES (1, 'Alex', 'Cole', 12)");
+$insert_people2 = mysqli_query($dbconnect, "INSERT INTO `people` VALUES (NULL, 'Nicole', 'Smith', 10)");
+$insert_people3 = mysqli_query($dbconnect, "INSERT INTO `people` VALUES (NULL, 'Bob', 'Smith', 14)");
+$insert_people4 = mysqli_query($dbconnect, "INSERT INTO `people` VALUES (NULL, 'Ellen', 'Jones', 16)");
+$delete_people1 = mysqli_query($dbconnect, "DELETE FROM `people` WHERE `id_person` = 3");
+$insert_hobbies1 = mysqli_query($dbconnect, "INSERT INTO `hobbies` VALUES (NULL, 'Sport', 'football or swimming')");
+$insert_hobbies2 = mysqli_query($dbconnect, "INSERT INTO `hobbies` VALUES (NULL, 'Computer games', 'Solitaire and Minesweeper')");
+$insert_hobbies3 = mysqli_query($dbconnect, "INSERT INTO `hobbies` VALUES (NULL, 'Reading', 'sci-fi and popular science')");
+$update_hobbies1 = mysqli_query($dbconnect, "UPDATE `hobbies` SET `Desription`='Solitaire, Minesweeper, Darkest Dungeon' WHERE `id_hobbies`=2");
+$join_sql = mysqli_query($dbconnect, "SELECT id_record, id_person, Surname, Age, Description FROM people_hobbies JOIN people ON people_hobbies.id_people = people.id_person");
 
-<select style="background-color: <?echo $colors_array[0];?>">
-    <option style="background-color: <?echo $colors_array[0];?>">Желтый</option>
-    <option style="background-color: <?echo $colors_array[1];?>">Красный</option>
-    <option style="background-color: <?echo $colors_array[2];?>">Голубой</option>
-</select>
+$alter_people_hobbies1 = mysqli_query($dbconnect, "ALTER TABLE `people_hobbies` ADD FOREIGN KEY (`id_hobbies`) REFERENCES `hobbies`(`id_hobbies`) ON DELETE RESTRICT ON UPDATE RESTRICT;");
+$alter_people_hobbies2 = mysqli_query($dbconnect, "ALTER TABLE `people_hobbies` ADD FOREIGN KEY (`id_people`) REFERENCES `people`(`id_person`) ON DELETE RESTRICT ON UPDATE RESTRICT;");
